c0e905106a8fe86c4c4bfebda0ad105869b67104
Jakub <staniojakub@gmail.com>
2020-03-10 18:54:27 +0100

    Initial react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>ToDos:</h1>
+      <ul>
+        <li>React router</li>
+        <li>Navigation bar</li>
+        <li>decide if material-ui or sc</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
